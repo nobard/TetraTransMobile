472d878a4186a44367f7bd99c82ab02fd8f1f263
nobard <sashayelizarov2001@gmail.com>
2023-01-04 22:11:39 +0500

    Сверстана страница заявок

--- PricesAndroid/PricesAndroid/Views/RequestsPage.xaml.cs
+++ PricesAndroid/PricesAndroid/Views/RequestsPage.xaml.cs
@@ -4,9 +4,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PricesAndroid.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using PricesAndroid.Models;
 
 namespace PricesAndroid.Views
 {
