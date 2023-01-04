3c3eb80588dd793b1508b7eac700fffd54043e89
nobard <sashayelizarov2001@gmail.com>
2023-01-04 10:40:04 +0500

    gitignore

---
+++ PricesAndroid/PricesAndroid/PricesAndroid/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PricesAndroid.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PricesAndroid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            
+            MainPage = new AppShell();
+        }
+
+
+        protected override void OnStart()
+        {
+            //Игнор темы системы(всегда светлая)
+            //Current.UserAppTheme = OSAppTheme.Light;
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PricesAndroid/PricesAndroid/PricesAndroid/AppShell.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PricesAndroid.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PricesAndroid
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            //Задаем стартовую страницу
+            CurrentItem = Estimation;
+
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(UserProfilePage), typeof(UserProfilePage));
+        }
+    }
+}
