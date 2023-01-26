ba87dcbe5bc8fdf982333611990de043e7594c78
nobar <sashayelizarov2001@gmail.com>
2023-01-26 09:01:26 +0500

    Закончен конвертер

--- PricesAndroid/PricesAndroid/Utilities/StatusToVisibleConverter.cs
+++ PricesAndroid/PricesAndroid/Utilities/StatusToVisibleConverter.cs
@@ -7,26 +7,20 @@ using Xamarin.Forms;
 
 namespace PricesAndroid.Utilities
 {
-    public class StatusToVisibleConverter<T> : IValueConverter
+    public class StatusToVisibleConverter : IValueConverter
     {
-        public T CurrStatusObject { get; set; }
+        //public T CurrStatusObject { get; set; }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (CurrStatusObject.ToString() == "Created") return (StatusEnum)value == StatusEnum.Created;
-            if (CurrStatusObject.ToString() == "InProgress") return (StatusEnum)value == StatusEnum.InProgress;
-            if (CurrStatusObject.ToString() == "Done") return (StatusEnum)value == StatusEnum.Done;
+            if (Enum.TryParse<StatusEnum>((string)parameter, out var status)) return (StatusEnum)value == status;
 
             return false;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (CurrStatusObject.ToString() == "Created") return (StatusEnum)value == StatusEnum.Created;
-            if (CurrStatusObject.ToString() == "InProgress") return (StatusEnum)value == StatusEnum.InProgress;
-            if (CurrStatusObject.ToString() == "Done") return (StatusEnum)value == StatusEnum.Done;
-
-            return false;
+            return (bool)value ? StatusEnum.InProgress : StatusEnum.InProgress;
         }
     }
 }
